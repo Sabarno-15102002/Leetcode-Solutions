c397f9c47f867d84981b66a453a7634cc6cbd070
Sabarno Biswas <sabarnobiswas2002@gmail.com>
2023-07-04 02:15:04 +0530

    Added solution - LeetHub

---
+++ Bridge edge in a graph - GFG/bridge-edge-in-a-graph.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    int timer = 1;
+     void dfs(int node, int parent, vector<int> adj[], vector<int>& vis, int tin[], int low[], int& ans, int c, int d){
+        vis[node]=1;
+        tin[node]=low[node]=timer;
+        timer++;
+        for(auto it:adj[node]){
+            if(it==parent)continue;
+            if(!vis[it]){
+                dfs(it, node, adj, vis, tin, low, ans, c, d);
+                low[node]=min(low[it], low[node]);
+                if(tin[node]<low[it] && it==d && node==c)
+                {
+                    ans=1;
+                }
+            }
+            else{
+                low[node]=min(low[node], low[it]);
+            }
+        }
+    }
+	public:
+    //Function to find if the given edge is a bridge in graph.
+    int isBridge(int V, vector<int> adj[], int c, int d) 
+    {
+        // Code here
+        vector<int> vis(V, 0);
+        int tin[V];
+        int low[V];
+        int ans=0;
+        dfs(c, -1, adj, vis, tin, low, ans, c, d);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back (v);
+            adj[v].push_back (u);
+        }
+        
+        int c,d;
+        cin>>c>>d;
+        
+        Solution obj;
+    	cout << obj.isBridge(V, adj, c, d) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
