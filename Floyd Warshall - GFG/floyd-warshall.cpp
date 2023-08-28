503eae1a0f0ed036035b4c265048c39acc5258b3
Sabarno Biswas <sabarnobiswas2002@gmail.com>
2023-08-28 20:37:19 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -11,35 +11,33 @@ class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
 	    // Code here
-	    int n=matrix.size();
-	    int m=matrix[0].size();
-	    
+	    int n = matrix.size();
 	    for(int i=0;i<n;i++){
-	        for(int j=0;j<m;j++){
-	            if(matrix[i][j]==-1){
-	                matrix[i][j]=1e9;
+	        for(int j=0;j<n;j++){
+	            if(matrix[i][j] == -1){
+	                matrix[i][j] = 1e9;
 	            }
+	            if(i==j) matrix[i][j] = 0;
 	        }
 	    }
 	    
-	    for(int via=0;via<m;via++){
+	    for(int k=0;k<n;k++){
 	        for(int i=0;i<n;i++){
-    	        for(int j=0;j<m;j++){
-    	            matrix[i][j]=min(matrix[i][j],matrix[i][via]+matrix[via][j]);
+    	        for(int j=0;j<n;j++){
+    	            matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
     	        }
     	    }
 	    }
 	    
 	    for(int i=0;i<n;i++){
-	        for(int j=0;j<m;j++){
-	            if(matrix[i][j]==1e9){
-	                matrix[i][j]=-1;
+	        for(int j=0;j<n;j++){
+	            if(matrix[i][j] == 1e9){
+	                matrix[i][j] = -1;
 	            }
 	        }
 	    }
 	}
-};;
-
+};
 
 //{ Driver Code Starts.
 int main(){
